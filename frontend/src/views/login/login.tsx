6b73d654104cee84dfd58961113bc27d89fd6759
zhangqingbo <zhangqingbo@yugeeker.com>
2025-11-25 18:25:40 +0800

    feat(前端): 添加登录页面布局和路径别名配置
    - 在tsconfig.json中添加路径别名配置，便于模块导入
    - 实现登录页面基础布局，包含表单区域和图片展示区域

--- frontend/src/views/login/login.tsx
+++ frontend/src/views/login/login.tsx
@@ -1,5 +1,15 @@
 const Login = () => {
-  return <div>登录/注册</div>;
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-[#575758]">
+      <main className="flex h-[80%] w-[80%] bg-[#030303]">
+        {/* 表单录入 */}
+        <section className="h-full flex-35 bg-[#030303]"></section>
+
+        {/* 图片展示 */}
+        <section className="h-full flex-65 bg-[#463737]"></section>
+      </main>
+    </div>
+  );
 };
 
 export default Login;
