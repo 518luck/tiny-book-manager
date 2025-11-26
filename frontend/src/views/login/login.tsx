7a3a7fa7a16e1c92c2e6ab769db261bbf1ba249c
zhangqingbo <zhangqingbo@yugeeker.com>
2025-11-26 14:55:00 +0800

    build: 添加axios依赖并初始化配置文件
    refactor: 调整utils.ts中的导入顺序和代码格式

--- frontend/src/views/login/login.tsx
+++ frontend/src/views/login/login.tsx
@@ -89,6 +89,7 @@ const Login = () => {
                 </p>
               )}
             </div>
+
             {/* 登录按钮 */}
             <Button type="submit" className="w-full">
               Sign In
