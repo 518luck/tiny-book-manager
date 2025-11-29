e848ab7517fc15f1c60f6381ff20ccdbd8f66777
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-11-29 22:31:18 +0800

    feat(书籍管理): 添加搜索框组件并替换占位文本
    使用InputGroup组件实现搜索功能，替换原有的占位文本

--- frontend/src/views/BookManage/Books.tsx
+++ frontend/src/views/BookManage/Books.tsx
@@ -1,9 +1,23 @@
+import { Search } from "lucide-react";
+
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group";
 import Book from "@/views/BookManage/components/Book";
 
 const Books = () => {
   return (
     <div className="h-full w-full">
-      <section className="bg-amber-100">搜索框</section>
+      <section className="m-2">
+        <InputGroup className="w-md">
+          <InputGroupAddon>
+            <Search />
+          </InputGroupAddon>
+          <InputGroupInput placeholder="搜索书籍" />
+        </InputGroup>
+      </section>
       <section className="grid grid-cols-2 gap-4 p-4 md:grid-cols-3 lg:grid-cols-4">
         {[...Array(10)].map((_, index) => (
           <Book key={index} />
