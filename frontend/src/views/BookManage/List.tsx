c6659f5feae786f32af92bc6def1d192c004c90e
蜻蜓ink队长 <165388484+518luck@users.noreply.github.com>
2025-11-29 18:15:38 +0800

    feat: 添加admin用户并更新图书管理界面样式
    - 在users.json中添加admin用户用于测试
    - 替换图书管理界面的图标并调整背景颜色和布局

--- frontend/src/views/BookManage/List.tsx
+++ frontend/src/views/BookManage/List.tsx
@@ -1,4 +1,4 @@
-import { ArrowBigRight } from "lucide-react";
+import { CircleUser } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
 import LeftSidebar from "@/views/BookManage/LeftSidebar";
@@ -7,7 +7,7 @@ const BookList = () => {
 
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-[#575758] text-stone-300">
-      <main className="flex h-[76%] w-[80%] bg-gray-800">
+      <main className="flex h-[76%] w-[80%] bg-[#212121]">
         {/* 左侧导航栏 */}
         <div className="h-full w-[16%]">
           <LeftSidebar />
@@ -15,10 +15,10 @@ const BookList = () => {
 
         {/* 右侧内容区域 */}
         <main className="w-full">
-          <div className="flex h-7 cursor-pointer justify-end bg-amber-200">
-            <ArrowBigRight onClick={() => navigate(-1)} />
+          <div className="flex h-11 cursor-pointer justify-end border-l border-[#333333] bg-gray-950 p-2">
+            <CircleUser onClick={() => navigate("/")} />
           </div>
-          图书
+          <section></section>
         </main>
       </main>
     </div>
